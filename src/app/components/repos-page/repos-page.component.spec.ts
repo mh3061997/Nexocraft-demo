cc706571a5b5833e53e333b4236be4b9ba4cf1ea
mhaitham <mhaitham@sumerge.com>
2021-11-13 19:03:05 +0200

    Added 3 Component Pages

---
+++ src/app/components/repos-page/repos-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReposPageComponent } from './repos-page.component';
+
+describe('ReposPageComponent', () => {
+  let component: ReposPageComponent;
+  let fixture: ComponentFixture<ReposPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReposPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReposPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
