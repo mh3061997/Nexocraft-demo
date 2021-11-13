d109e01039459d5bdb2baf81d450dc3277cd8bb7
mhaitham <mhaitham@sumerge.com>
2021-11-13 19:01:37 +0200

    configured routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReposPageComponent } from './components/repos-page/repos-page.component';
+import { StatsPageComponent } from './components/stats-page/stats-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "repos", component: ReposPageComponent,
+  }, {
+    path: "stats", component: StatsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
