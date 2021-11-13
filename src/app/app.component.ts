f1ade49903dbf1ddead1691c86b9239beff7579a
mhaitham <mhaitham@sumerge.com>
2021-11-13 18:17:13 +0200

    removed unneeded getters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Repository } from './models/repository.model';
+import { GithubRestClientService } from './services/github-rest-client.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nexocraft';
+  arr: Repository[] = [];
+
+  constructor(private githubRestClient: GithubRestClientService) {
+
+    this.githubRestClient.getUserRepositories("mh3061997").subscribe((res) => {
+      this.arr = res;
+      console.log(this.arr);
+      
+    })
+  }
 }
--- src/app/models/repository.model.ts
+++ src/app/models/repository.model.ts
@@ -5,13 +5,4 @@ export class Repository {
            private stargazers_count: number,
            private  language: string) { }
 
-getName():string{
-     return this.name;
-}
-getStarCount():number{
-     return this.stargazers_count;
-}
-getLanguage():string{
-     return this.language;
-}
 };
