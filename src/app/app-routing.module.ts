8013d18d907deb7807e6c13324e17d195bde9c79
mhaitham <mhaitham@sumerge.com>
2021-11-13 19:02:46 +0200

    Configuted Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReposPageComponent } from './components/repos-page/repos-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 import { StatsPageComponent } from './components/stats-page/stats-page.component';
 
 const routes: Routes = [
@@ -8,6 +9,8 @@ const routes: Routes = [
     path: "repos", component: ReposPageComponent,
   }, {
     path: "stats", component: StatsPageComponent
+  }, {
+    path: "search", component: SearchPageComponent
   }
 ];
 
