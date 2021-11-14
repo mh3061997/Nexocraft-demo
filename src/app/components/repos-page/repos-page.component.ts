56bee01f2f65f2c9d4b17dd9a1b8b70406696b10
mhaitham <mhaitham@sumerge.com>
2021-11-14 16:58:33 +0200

    Changed Repos Fetching to repos page

--- src/app/components/repos-page/repos-page.component.ts
+++ src/app/components/repos-page/repos-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Repository } from 'src/app/models/repository.model';
+import { GithubRestClientService } from 'src/app/services/github-rest-client.service';
 
 @Component({
   selector: 'app-repos-page',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReposPageComponent implements OnInit {
 
-  constructor() { }
+  repos: Repository[] = [];
+
+  constructor(
+    private router: Router,
+    private currentRoute: ActivatedRoute,
+    private githubRestClient: GithubRestClientService) {
+
+    let username: string;
+    this.currentRoute.queryParams.subscribe(params => {
+      username = params.username
+      if (!username) {
+      this.router.navigate([".."]);
+      }
+    });
+
+    this.githubRestClient.getUserRepositories(username!).subscribe(repos => {
+      this.repos = repos;
+    });
+  }
 
   ngOnInit(): void {
+    this.repos = history.state.repos;
+    console.log("repos are ", this.repos);
+
   }
 
 }
