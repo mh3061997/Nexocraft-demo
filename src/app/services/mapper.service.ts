f018c0f397158a0ff5d12d686ec69c7cd9472987
mhaitham <mhaitham@sumerge.com>
2021-11-15 14:18:35 +0200

    Added Mapper

--- src/app/services/mapper.service.ts
+++ src/app/services/mapper.service.ts
@@ -10,42 +10,30 @@ export class MapperService {
 
   constructor() { }
 
+  formatCommitActivityArray(arr: Activity[]): number[][] {
+    return arr.map(activity => {
+      //multiply by 1000 because we need data in milliseconds and github api returns it in seconds
+      return [activity.week * 1000, activity.total];
+    })
+  }
+
   formatParticipationArray(participation: Participation): SeriesOptionsType[] {
 
-
     let seriesOptionsArr: SeriesOptionsType[] = [];
-
     let series: SeriesOptionsType = {
       name: "owner",
       type: "line",
       data: participation.owner
     }
-
     seriesOptionsArr.push(series);
-
+    
     series = {
       name: "all",
       type: "line",
       data: participation.all
     }
-
     seriesOptionsArr.push(series);
 
     return seriesOptionsArr;
   }
-
-  // formatCommitActivityArrayData(activityArr: Activity[]){
-
-   
-  //   return activityArr.map(activity=>{
-  //     return activity.total;
-  //   });
-  // }
-  // formatCommitActivityArrayCategories(activityArr: Activity[]){
-
-   
-  //   return activityArr.map(activity=>{
-  //     return activity.week;
-  //   });
-  // }
 }
