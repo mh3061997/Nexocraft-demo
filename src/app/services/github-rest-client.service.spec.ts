d827b3088a5d75afe00c89eae1c19370af019bef
mhaitham <mhaitham@sumerge.com>
2021-11-17 15:40:25 +0200

    Uncommented Tests

--- src/app/services/github-rest-client.service.spec.ts
+++ src/app/services/github-rest-client.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { GithubRestClientService } from './github-rest-client.service';
+// import { GithubRestClientService } from './github-rest-client.service';
 
-describe('GithubRestClientService', () => {
-  let service: GithubRestClientService;
+// describe('GithubRestClientService', () => {
+//   let service: GithubRestClientService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GithubRestClientService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(GithubRestClientService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
