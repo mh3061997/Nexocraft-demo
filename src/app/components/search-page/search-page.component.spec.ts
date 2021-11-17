e641443735fa43c3a17961c891971c2550a5397e
mhaitham <mhaitham@sumerge.com>
2021-11-17 15:40:07 +0200

    Added Search Tests

--- src/app/components/search-page/search-page.component.spec.ts
+++ src/app/components/search-page/search-page.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SearchPageComponent } from './search-page.component';
 
 describe('SearchPageComponent', () => {
   let component: SearchPageComponent;
   let fixture: ComponentFixture<SearchPageComponent>;
-
+    let router:Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchPageComponent ]
+      declarations: [ SearchPageComponent ],
+      imports:[RouterTestingModule,FormsModule],
     })
     .compileComponents();
+
+    router= TestBed.inject(Router);
   });
 
   beforeEach(() => {
@@ -21,5 +27,11 @@ describe('SearchPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate correctly',()=>{
+    component.navigateReposPage("spring");
+    fixture.whenStable().then(() => { expect(router.navigate).toHaveBeenCalledWith(['search']); })
+
   });
 });
